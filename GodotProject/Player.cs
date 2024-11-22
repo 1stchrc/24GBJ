c2ea9d4ea17baa29ca3ca24f14fe287ac1020830
1stchrc <1206833509@qq.com>
2024-11-23 01:46:10 +0800

    minor bug fix

--- GodotProject/Player.cs
+++ GodotProject/Player.cs
@@ -10,7 +10,8 @@ namespace Fcc{
         static float hAccel => 3000.0f;
         static float hDrag => 1000.0f;
         static float airHMultiplier => 0.7f;
-        static float jumpVel => 400.0f;
+        static float jumpHVel => 50.0f;
+        static float jumpVVel => 400.0f;
         static float gravity => 2500.0f;
         static float maxFallSpeed => 500.0f;
         static float jumpGravityMultiplier => 0.15f;
@@ -18,6 +19,7 @@ namespace Fcc{
         static int wolfFrames => 6;
         static int preFrames => 6;
         
+
         public static async void Possess(
             CharacterBody2D bodyb, 
             CharacterBody2D soulb, 
@@ -30,6 +32,7 @@ namespace Fcc{
             var jmpCounter = new EventCounter<int>(counterEv);
 
             CharacterBody2D cb = bodyb;
+            CharacterBody2D bb = null;
             soulb.GetParent().RemoveChild(soulb);
             bool isSoulForm = false;
             for(;;){
@@ -40,41 +43,59 @@ namespace Fcc{
                         soulb.Transform = new Transform2D(bodyb.Transform.X, bodyb.Transform.Y, bodyb.Transform.Origin);
                         soulb.Velocity = bodyb.Velocity;
                         cb = soulb;
+                        bb = bodyb;
                         isSoulForm = true;
                     }else{
                         var dis = soulb.Transform.Origin - bodyb.Transform.Origin;
                         float sqrmgn = dis.X * dis.X + dis.Y + dis.Y;
-                        if(sqrmgn <= 10000.0f){
+                        if(sqrmgn <= 64.0f * 64.0f){
                             soulb.GetParent().RemoveChild(soulb);
                             cb = bodyb;
+                            bb = null;
                             isSoulForm = false;
+                            wolfCounter.Rewind(0);
+                            jmpCounter.Rewind(0);
                         }
                     }
                 }
-                Vector2 vel = cb.Velocity;
                 {
+                    Vector2 vel = cb.Velocity;
                     float hAxis = Input.GetAxis("ui_left", "ui_right");
+                    {
+                        int sgn = Mathf.Sign(vel.X);
+                        if(sgn == 0)sgn = Mathf.Sign(hAxis);
+                        float abVelX = sgn * vel.X;
+                        float dv = dt * (sgn * hAccel * hAxis - hDrag) * (cb.IsOnFloor() ? 1.0f : airHMultiplier);
+                        abVelX = Mathf.Clamp(abVelX + dv, 0, maxHSpeed);
+                        vel.X = abVelX * sgn;
+                    }
+                    counterEv.Emit(0);
+                    if(cb.IsOnFloor())wolfCounter.Rewind(wolfFrames);
+                    if(Input.IsActionJustPressed("ui_accept"))preCounter.Rewind(preFrames);
+                    if(!wolfCounter.elapsed && !preCounter.elapsed){
+                        wolfCounter.Rewind(0);
+                        preCounter.Rewind(0);
+                        jmpCounter.Rewind(jumpFrames);
+                        vel.Y = -jumpVVel;
+                        vel.X += hAxis *jumpHVel;
+                    }
+                    if(!Input.IsActionPressed("ui_accept"))jmpCounter.Rewind(0);
+                    vel.Y += dt * gravity * (jmpCounter.elapsed ? 1.0f : jumpGravityMultiplier);
+                    vel.Y = Mathf.Clamp(vel.Y, -Mathf.Inf, maxFallSpeed);
+                    cb.Velocity = vel;
+                    cb.MoveAndSlide();
+                }
+                if(bb != null){
+                    Vector2 vel = bb.Velocity;
                     int sgn = Mathf.Sign(vel.X);
-                    if(sgn == 0)sgn = Mathf.Sign(hAxis);
                     float abVelX = sgn * vel.X;
-                    float dv = dt * (sgn * hAccel * hAxis - hDrag) * (cb.IsOnFloor() ? 1.0f : airHMultiplier);
+                    float dv = -dt * hDrag * (cb.IsOnFloor() ? 1.0f : airHMultiplier);
                     abVelX = Mathf.Clamp(abVelX + dv, 0, maxHSpeed);
                     vel.X = abVelX * sgn;
+                    vel.Y += dt * gravity;
+                    bb.Velocity = vel;
+                    bb.MoveAndSlide();
                 }
-                counterEv.Emit(0);
-                if(cb.IsOnFloor())wolfCounter.Rewind(wolfFrames);
-                if(Input.IsActionJustPressed("ui_accept"))preCounter.Rewind(preFrames);
-                if(!wolfCounter.elapsed && !preCounter.elapsed){
-                    wolfCounter.Rewind(0);
-                    preCounter.Rewind(0);
-                    jmpCounter.Rewind(jumpFrames);
-                    vel.Y = -jumpVel;
-                }
-                if(!Input.IsActionPressed("ui_accept"))jmpCounter.Rewind(0);
-                vel.Y += dt * gravity * (jmpCounter.elapsed ? 1.0f : jumpGravityMultiplier);
-                vel.Y = Mathf.Clamp(vel.Y, -Mathf.Inf, maxFallSpeed);
-                cb.Velocity = vel;
-                cb.MoveAndSlide();
             }
         }
     }

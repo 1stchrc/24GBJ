8f408f6ec64832fb12765c7906c84dd67a01259b
fireg958 <qadkxz@163.com>
2024-12-01 18:09:43 +0800

    level 4

--- GodotProject/Scripts/Level/LevelLoader.cs
+++ GodotProject/Scripts/Level/LevelLoader.cs
@@ -57,7 +57,7 @@ public partial class LevelLoader : Node{
 	}
 	private Timer _timer;
 	void LoadCurLevel(){
-		GetChild<AnimatedSprite2D>(1).Play("cutscene")；
+		//GetChild<AnimatedSprite2D>(1).Play("cutscene")；
 		levelLoaded = curLevelScene.Instantiate<GeneralLevel>();
 		levelLoaded.loader = this;
 		WhereLevelInitializedAt.CallDeferred(MethodName.AddChild, levelLoaded);
